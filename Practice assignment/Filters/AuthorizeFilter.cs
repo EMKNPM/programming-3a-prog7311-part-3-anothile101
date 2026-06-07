01619950408369ad798972590546b62178964048
Anothile <st10440981@vccconnect.edu.za>
2026-06-07 17:32:54 +0200

    Third Commit: Uanuthenticated requests will throw an unauthorsied status rather than returning unfiltered data

---
+++ Practice assignment/Filters/AuthorizeFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Practice_assignment.Filters;
+
+public class AuthorizeFilter : IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        var token = context.HttpContext.Session.GetString("JwtToken");
+
+        // Allow access to Login page without token
+        var path = context.HttpContext.Request.Path.ToString().ToLower();
+        if (path.Contains("/account/login"))
+        {
+            return;
+        }
+
+        // Redirect to login if no token
+        if (string.IsNullOrEmpty(token))
+        {
+            context.Result = new RedirectToActionResult("Login", "Account", null);
+        }
+    }
+}
